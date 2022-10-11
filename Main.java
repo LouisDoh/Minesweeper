dd50c1b910b570cca56212da296cc459eba02c5e
Louis Doherty <louisdoherty98@gmail.com>
2022-10-11 17:23:33 +0100

    Added flagging functionality to Grid class.

--- Main.java
+++ Main.java
@@ -1,9 +1,11 @@
 public class Main {
     public static void main(String[] args) {
 
-        Grid gameGrid = new Grid(5,10);
+        Grid gameGrid = new Grid(10,10);
         System.out.println(gameGrid);
         gameGrid.makeMove(0,0);
+        System.out.println(gameGrid);
+        gameGrid.changeTileFlagged(8,8,true);
         System.out.println(gameGrid);
     }
 }
