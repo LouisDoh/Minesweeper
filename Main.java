429434a599e8cdabd766b84848f2be9ca40165d2
Louis Doherty <louisdoherty98@gmail.com>
2022-10-12 16:34:08 +0100

    Added input checks for all user input.

--- Main.java
+++ Main.java
@@ -16,11 +16,23 @@ public class Main {
         boolean win;
         int gridSize;
         int noOfBombs;
+        String againInput;
+        boolean askingForAgain;
 
         while(playAgain) {
             System.out.println("How big would you like your grid?");
+            while(!in.hasNextInt()) {
+                System.out.println("Please only input a single number. How big would you like your grid?");
+                in.next();
+            }
             gridSize = in.nextInt();
+
             System.out.println("And how many bombs do you want?");
+            while(!in.hasNextInt()) {
+                System.out.println("Please only input a single number. How many bombs do you want?");
+                in.next();
+            }
+
             noOfBombs = in.nextInt();
             in.nextLine(); //clean input stream
 
@@ -48,9 +60,20 @@ public class Main {
                 System.out.println("You clicked on a bomb and lost! Loser!");
             }
             System.out.println("Would you like to play again (y/n)?");
-            if(in.next().equals("n")) {
-                playAgain = false;
+            askingForAgain = true;
+
+            while(askingForAgain) {
+                againInput = in.next();
+                if(againInput.equals("n")) {
+                    playAgain = false;
+                    askingForAgain = false;
+                } else if(!againInput.equals("y")) {
+                    System.out.println("Please only input a single letter, y or n:");
+                } else {
+                    askingForAgain = false;
+                }
             }
+
             in.nextLine(); //clean the input stream
         }
 
